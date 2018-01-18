139516629fdde0006516cca543336702a360aabc
jlouie <jlouie@foundationmedicine.com>
2018-01-18 14:56:21 -0500

    removed chown_mounts from runlist, and set owner in NFS options

--- cyclecloud_projects/chainlink/specs/default/chef/roles/chainlink_master_role.rb
+++ cyclecloud_projects/chainlink/specs/default/chef/roles/chainlink_master_role.rb
@@ -1,12 +1,11 @@
 name "chainlink_master_role"
 description "Chainlink Master Role"
-run_list("role[scheduler]",   
+run_list("role[scheduler]",
   "recipe[createusers]",
   "recipe[cyclecloud]",
   "recipe[cshared::directories]",
   "recipe[cuser]",
   "recipe[cshared::server]",
-  "recipe[createusers::chown_mounts]",
   "recipe[uge::master]",
   "recipe[cganglia::server]",
   "recipe[cluster_init]")
