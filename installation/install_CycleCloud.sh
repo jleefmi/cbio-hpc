0fe403fd077e62507476b692376ea2ab02e6d120
Wing Ng <wng@foundationmedicine.com>
2018-03-20 15:19:25 -0400

    Add installation script

---
+++ installation/install_CycleCloud.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+   set -x
+   set -e
+
+   export CS_HOME=/opt/cycle_server
+   export CS_VERSION=6.6.0
+   export INSTALL_DIR=/tmp/cyclecloud
+   mkdir -p ${INSTALL_DIR}
+   chmod a+rwX ${INSTALL_DIR}
+
+   echo "Bootstrapping CycleCloud..."
+   echo "Fetching CycleCloud Bootstrap script..."
+
+   yum -y update
+   yum -y install epel-release
+   yum install -y python-pip java-1.8.0-openjdk.x86_64
+
+   pip install -U pip awscli pystache argparse python-daemon requests
+
+   cd ${INSTALL_DIR}
+   aws s3 cp --recursive s3://fm-ae1-cyclecloud-poc/installers/${CS_VERSION}/ .
+   tar xzf cyclecloud*tar.gz
+   tar xzf pogo*tar.gz
+   tar xzf cycle_server-all*tar.gz
+   mv cyclecloud /usr/local/bin/
+   mv pogo /usr/local/bin/
+
+   pushd cycle_server/
+   ./install.sh --nostart
+   popd
+
+   chown cycle_server:cycle_server ${INSTALL_DIR}/cyclecloud_init.txt
+   chown cycle_server:cycle_server ${INSTALL_DIR}/users_init.txt
+   cp ${INSTALL_DIR}/cyclecloud_init.txt ${CS_HOME}/config/data
+   cp ${INSTALL_DIR}/users_init.txt ${CS_HOME}/config/data
+
+   cd ${CS_HOME}
+   sed -i '/^webServerMaxHeapSize/c webServerMaxHeapSize=8192M' ${CS_HOME}/config/cycle_server.properties
+   sed -i '/^webServerPort/c webServerPort=80' ${CS_HOME}/config/cycle_server.properties
+   sed -i '/^webServerSslPort/c webServerSslPort=443' ${CS_HOME}/config/cycle_server.properties
+   sed -i '/^brokerMaxHeapSize/c brokerMaxHeapSize=2048M' ${CS_HOME}/config/cycle_server.properties
+
+   echo "Starting CycleCloud..."
+   ${CS_HOME}/cycle_server start --wait
