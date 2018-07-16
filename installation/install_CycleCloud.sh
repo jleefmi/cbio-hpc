7df219d6e65660319e5e35b9a892a704692db759
jlouie <jlouie@foundationmedicine.com>
2018-07-16 15:26:45 -0400

    added variable use to install_CycleCloud.sh

--- installation/install_CycleCloud.sh
+++ installation/install_CycleCloud.sh
@@ -1,60 +1,63 @@
 #!/bin/bash
 
-   set -x
-   set -e
+set -x
+set -e
 
-   export CS_HOME=/opt/cycle_server
-   export CS_VERSION=6.6.0
-   export INSTALL_DIR=/tmp/cyclecloud
-   mkdir -p ${INSTALL_DIR}
-   chmod a+rwX ${INSTALL_DIR}
+export CS_HOME=/opt/cycle_server
+export INSTALL_DIR=/tmp/cyclecloud
 
-   echo "Bootstrapping CycleCloud..."
-   echo "Fetching CycleCloud Bootstrap script..."
+export CS_VERSION=6.6.0
+export CYCLECLOUD_LOCKER=fm-ae1-cyclecloud-poc
 
-   yum -y update
-   yum -y install epel-release
-   yum install -y python-pip java-1.8.0-openjdk.x86_64
+mkdir -p ${INSTALL_DIR}
+chmod a+rwX ${INSTALL_DIR}
 
-   pip install -U pip awscli pystache argparse python-daemon requests
+echo "Bootstrapping CycleCloud..."
+echo "Fetching CycleCloud Bootstrap script..."
 
-   cd ${INSTALL_DIR}
-   aws s3 cp --recursive s3://fm-ae1-cyclecloud-poc/installers/${CS_VERSION}/ .
-   tar xzf cyclecloud*tar.gz
-   tar xzf pogo*tar.gz
-   tar xzf cycle_server-all*tar.gz
-   mv cyclecloud /usr/local/bin/
-   mv pogo /usr/local/bin/
+yum -y update
+yum -y install epel-release
+yum install -y python-pip java-1.8.0-openjdk.x86_64
 
-   pushd cycle_server/
-   ./install.sh --nostart
-   popd
+pip install -U pip awscli pystache argparse python-daemon requests
 
-   chown cycle_server:cycle_server ${INSTALL_DIR}/cyclecloud_init.txt
-   chown cycle_server:cycle_server ${INSTALL_DIR}/users_init.txt
-   cp ${INSTALL_DIR}/cyclecloud_init.txt ${CS_HOME}/config/data
-   cp ${INSTALL_DIR}/users_init.txt ${CS_HOME}/config/data
+cd ${INSTALL_DIR}
+aws s3 cp --recursive s3://${CYCLECLOUD_LOCKER}/installers/${CS_VERSION}/ .
+tar xzf cyclecloud*tar.gz
+tar xzf pogo*tar.gz
+tar xzf cycle_server-all*tar.gz
+mv cyclecloud /usr/local/bin/
+mv pogo /usr/local/bin/
 
-   keytool -genkey \
-   -keyalg RSA \
-   -sigalg SHA256withRSA \
-   -alias CycleServer \
-   -dname "CN=Foundation Medicine, OU=IT, O=Foundation Medicine, L=Cambridge, ST=MA, C=US" \
-   -keypass "SelfSignedUseOnlyPlease" \
-   -keystore .keystore \
-   -storepass "SelfSignedUseOnlyPlease"
+pushd cycle_server/
+./install.sh --nostart
+popd
 
-   mv .keystore ${CS_HOME}/
-   chown cycle_server:cycle_server ${CS_HOME}/.keystore
+chown cycle_server:cycle_server ${INSTALL_DIR}/cyclecloud_init.txt
+chown cycle_server:cycle_server ${INSTALL_DIR}/users_init.txt
+cp ${INSTALL_DIR}/cyclecloud_init.txt ${CS_HOME}/config/data
+cp ${INSTALL_DIR}/users_init.txt ${CS_HOME}/config/data
 
-   cd ${CS_HOME}
-   sed -i 's/^webServerKeystorePass=changeit/webServerKeystorePass=SelfSignedUseOnlyPlease/' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^webServerMaxHeapSize/c webServerMaxHeapSize=8192M' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^webServerPort/c webServerPort=80' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^webServerSslPort/c webServerSslPort=443' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^brokerMaxHeapSize/c brokerMaxHeapSize=2048M' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^webServerEnableHttp/c webServerEnableHttp=false' ${CS_HOME}/config/cycle_server.properties
-   sed -i '/^webServerEnableHttps/c webServerEnableHttps=true' ${CS_HOME}/config/cycle_server.properties
+keytool -genkey \
+-keyalg RSA \
+-sigalg SHA256withRSA \
+-alias CycleServer \
+-dname "CN=Foundation Medicine, OU=IT, O=Foundation Medicine, L=Cambridge, ST=MA, C=US" \
+-keypass "SelfSignedUseOnlyPlease" \
+-keystore .keystore \
+-storepass "SelfSignedUseOnlyPlease"
 
-   echo "Starting CycleCloud..."
-   ${CS_HOME}/cycle_server start --wait
+mv .keystore ${CS_HOME}/
+chown cycle_server:cycle_server ${CS_HOME}/.keystore
+
+cd ${CS_HOME}
+sed -i 's/^webServerKeystorePass=changeit/webServerKeystorePass=SelfSignedUseOnlyPlease/' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^webServerMaxHeapSize/c webServerMaxHeapSize=8192M' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^webServerPort/c webServerPort=80' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^webServerSslPort/c webServerSslPort=443' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^brokerMaxHeapSize/c brokerMaxHeapSize=2048M' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^webServerEnableHttp/c webServerEnableHttp=false' ${CS_HOME}/config/cycle_server.properties
+sed -i '/^webServerEnableHttps/c webServerEnableHttps=true' ${CS_HOME}/config/cycle_server.properties
+
+echo "Starting CycleCloud..."
+${CS_HOME}/cycle_server start --wait
